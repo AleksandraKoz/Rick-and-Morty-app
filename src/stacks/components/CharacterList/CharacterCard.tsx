21c7634a87544e4f31f673cd07d63e382557f336
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-19 22:37:22 +0200

    Add base version of CharacterCard component

---
+++ src/stacks/components/CharacterList/CharacterCard.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Image, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { useCharactersContext } from '../../../context/CharactersContext';
+import { MainStackNavigationProp } from '../../Main/Main.routes';
+import { Character } from '../../../api/characters';
+import Wrapper from "../Base/Wrapper/Wrapper";
+import { styles } from './CharacterListComponents.styled';
+import CharacterCardData from "./CharacterCardData";
+import LikeButton from "./LikeButton";
+
+type TCharacterListCard = {
+  characterData: Character;
+};
+
+const CharacterCard = ({ characterData }: TCharacterListCard) => {
+  const { setSelectedCharacter } = useCharactersContext();
+  const { navigate } = useNavigation<MainStackNavigationProp>();
+  
+  const handleCardClick = (character: Character) => {
+    setSelectedCharacter(character);
+    navigate('CharacterDetailsStack', {
+      screen: 'CharacterDetailsScreen',
+    });
+  };
+  
+  return (
+    <TouchableOpacity onPress={() => handleCardClick(characterData)}>
+      <Wrapper style={styles.cardContainer}>
+        <CharacterCardData characterData={characterData}/>
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: characterData.image }} style={styles.avatar}/>
+          <LikeButton/>
+        </View>
+      </Wrapper>
+    </TouchableOpacity>
+  );
+};
+
+export default CharacterCard;
