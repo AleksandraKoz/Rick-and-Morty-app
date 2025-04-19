6ed9ae32bda36fe19f42ac17f41b8b422b8855e9
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-19 13:43:59 +0200

    Add screen header to Character List screen

--- src/stacks/TabNavigation/screens/CharacterList/CharacterList.screen.tsx
+++ src/stacks/TabNavigation/screens/CharacterList/CharacterList.screen.tsx
@@ -1,23 +1,29 @@
-import {View, Text, Button} from 'react-native';
+import {Button, SafeAreaView, View} from 'react-native';
 import React from 'react';
 import {styles} from './CharacterList.styled';
 import {useNavigation} from '@react-navigation/native';
 import {MainStackNavigationProp} from '../../../Main/Main.routes';
 
+import NavBar from "../../../components/Base/NavBar/NavBar";
+import TitleText from "../../../components/Base/TitleText/TitleText";
+
 const CharacterListScreen = () => {
   const {navigate} = useNavigation<MainStackNavigationProp>();
   return (
-    <View style={styles.container}>
-      <Text>Implement CharactersListScreen</Text>
-      <Button
-        title="Navigate to Details screen"
-        onPress={(): void => {
-          navigate('CharacterDetailsStack', {
-            screen: 'CharacterDetailsScreen',
-          });
-        }}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <NavBar/>
+      <View style={styles.container}>
+        <TitleText title="Characters"/>
+        <Button
+          title="Navigate to Details screen"
+          onPress={(): void => {
+            navigate('CharacterDetailsStack', {
+              screen: 'CharacterDetailsScreen',
+            });
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
