41d0d882d16f31c52767db92afe87929f4c52b9f
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-19 22:44:24 +0200

    Add styles for CharacterList components

---
+++ src/stacks/components/CharacterList/CharacterListComponents.styled.ts
@@ -0,0 +1,60 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  cardContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  infoContainer: {
+    margin: 10,
+    flex: 1,
+  },
+  titleText: {
+    fontSize: 12,
+    color: '#59695C',
+    fontWeight: '500',
+    letterSpacing: 1
+  },
+  nameText: {
+    fontSize: 16,
+    flexWrap: 'wrap',
+    color: '#162C1B',
+    fontWeight: '400',
+    marginTop: 4,
+    marginBottom: 18
+  },
+  imageContainer: {
+    width: 200,
+    height: 200,
+    borderRadius: 24,
+    overflow: 'hidden',
+    position: 'relative',
+  },
+  avatar: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 24,
+    borderWidth: 1,
+    borderColor: '#224229',
+    backgroundColor: '#224229',
+  },
+  likeButton: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    backgroundColor: '#F4F6F4',
+    borderColor: '#224229',
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  likeText: {
+    marginLeft: 6,
+    color: '#162C1B',
+    fontWeight: '400',
+    fontSize: 14,
+  },
+});
