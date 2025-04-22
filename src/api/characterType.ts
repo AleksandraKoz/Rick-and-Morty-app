2ed1b6fe3451cae044720a2680609671b73e28d6
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-22 14:37:02 +0200

    Move character type to separate file

---
+++ src/api/characterType.ts
@@ -0,0 +1,27 @@
+export enum characterStatus {
+  Alive = 'Alive',
+  Dead = "Dead",
+  Unknown = 'unknown'
+}
+
+export enum characterGender {
+  Female = 'Female',
+  Male = "Male",
+  Genderless = "Genderless",
+  Unknown = 'unknown'
+}
+
+export type Character = {
+  id: number;
+  name: string;
+  status: characterStatus;
+  species: string;
+  type: string;
+  gender: characterGender;
+  origin: Record<string, string>;
+  location: Record<string, string>;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
--- src/api/characters.ts
+++ src/api/characters.ts
@@ -1,29 +1,5 @@
 import axios from 'axios';
-
-enum characterStatus {
-  Alive = 'Alive',
-  Dead = "Dead",
-  Unknown = 'unknown'
-}
-
-enum characterGender {
-  Female = 'Female',
-  Male = "Male",
-  Genderless = "Genderless",
-  Unknown = 'unknown'
-}
-
-export type Character = {
-  id: number;
-  name: string;
-  status: characterStatus;
-  species: string;
-  type: string;
-  gender: characterGender;
-  origin: Record<string, string>;
-  location: Record<string, string>;
-  image: string;
-};
+import { Character } from './characterType';
 
 type CharactersResponse = {
   results: Character[];
