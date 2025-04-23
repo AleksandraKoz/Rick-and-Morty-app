00ced3df14bd1a1b261431221d4597a743aa550c
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-23 19:21:38 +0200

    Modify Search Bar component to pass data inside

--- src/stacks/components/Base/SearchBar/SearchBar.tsx
+++ src/stacks/components/Base/SearchBar/SearchBar.tsx
@@ -1,33 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TextInput, TouchableHighlight, View } from 'react-native';
 import EvilIcons from '@expo/vector-icons/EvilIcons';
 
 import { styles } from './SearchBar.styles';
 
-const SearchBar = () => {
-  const [ query, setQuery ] = useState('');
-  
-  return (
-    <View style={styles.container}>
-      <EvilIcons name="search" size={24} color="#162C1B"/>
-      <TextInput
-        style={styles.input}
-        placeholder="Search the characters"
-        placeholderTextColor="#59695C"
-        value={query}
-        onChangeText={setQuery}
-      />
-      {query.length > 0 && (
-        <TouchableHighlight
-          style={styles.clearButton}
-          underlayColor="#DAE4DC"
-          onPress={() => setQuery('')}
-        >
-          <EvilIcons name="close" size={24} color="#59695C"/>
-        </TouchableHighlight>
-      )}
-    </View>
-  );
+interface ISearchBar {
+  query: string;
+  onChangeQuery: (text: string) => void;
 };
+
+const SearchBar = ({ query, onChangeQuery }: ISearchBar) => (
+  <View style={styles.container}>
+    <EvilIcons name="search" size={24} color="#162C1B"/>
+    <TextInput
+      style={styles.input}
+      placeholder="Search the characters"
+      placeholderTextColor="#59695C"
+      value={query}
+      onChangeText={onChangeQuery}
+    />
+    {query.length > 0 && (
+      <TouchableHighlight
+        style={styles.clearButton}
+        underlayColor="#DAE4DC"
+        onPress={() => onChangeQuery('')}
+      >
+        <EvilIcons name="close" size={24} color="#59695C"/>
+      </TouchableHighlight>
+    )}
+  </View>
+);
 
 export default SearchBar;
