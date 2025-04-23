f1309b9ac271c2fe1b317c92dcafc123a0852e2d
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-23 14:11:51 +0200

    Add base version of CharacterDetailsScreen

--- src/stacks/CharacterDetails/screens/CharacterDetails/CharacterDetails.screen.tsx
+++ src/stacks/CharacterDetails/screens/CharacterDetails/CharacterDetails.screen.tsx
@@ -1,12 +1,37 @@
-import {Text, View} from 'react-native';
 import React from 'react';
-import {styles} from './CharacterDetails.styled';
+import { Image, SafeAreaView, View } from 'react-native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+
+import NavBar from '../../../components/Base/NavBar/NavBar';
+import Wrapper from '../../../components/Base/Wrapper/Wrapper';
+import { styles } from './CharacterDetails.styled';
+import CharacterName from '../../../components/CharacterDetails/CharacterName';
+import WideLikeButton from '../../../components/CharacterDetails/WideLikeButton';
+import CharacterDetailsDataSection from '../../../components/CharacterDetails/CharacterDetailsDataSection';
+import { CharacterDetailsStackNavigationProp, CharacterDetailsStackParamList } from "../../CharacterDetails.routes";
+
+type DetailsRouteProp = RouteProp<
+  CharacterDetailsStackParamList,
+  'CharacterDetailsScreen'
+>;
 
 const CharacterDetailsScreen = () => {
+  const navigation = useNavigation<CharacterDetailsStackNavigationProp>();
+  const { params } = useRoute<DetailsRouteProp>();
+  const { character } = params
+  
   return (
-    <View style={styles.container}>
-      <Text>Implement CharacterDetailsScreen</Text>
-    </View>
+    <SafeAreaView style={styles.background}>
+      <NavBar/>
+      <View style={styles.container}>
+        <Wrapper style={{ padding: 24 }}>
+          <Image source={{ uri: character.image }} style={styles.avatar}/>
+          <CharacterName name={character.name}/>
+          <CharacterDetailsDataSection characterData={character}/>
+          <WideLikeButton/>
+        </Wrapper>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- src/stacks/CharacterDetails/screens/CharacterDetails/CharacterDetails.styled.ts
+++ src/stacks/CharacterDetails/screens/CharacterDetails/CharacterDetails.styled.ts
@@ -1,9 +1,21 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: '#162C1B'
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: '#F4F6F5'
+  },
+  avatar: {
+    width: '100%',
+    aspectRatio: 1,
+    resizeMode: 'cover',
+    borderRadius: 24,
+    borderWidth: 1,
+    borderColor: '#224229',
   },
 });
