e2dee7543d835bbd7801b4eea564046218dfc868
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-25 19:34:45 +0200

    Update useCharacters hook and API request to enable additional filtration

--- src/hooks/useCharacters.ts
+++ src/hooks/useCharacters.ts
@@ -1,19 +1,30 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { CharactersResponse, fetchCharactersByPage } from '../api/characters';
 
-export const useCharacters = (searchedName: string = '') =>
+export const useCharacters = (
+  searchedName: string = '',
+  filteredStatus: string = '',
+  filteredSpecies: string = '',
+) =>
   useInfiniteQuery<
     CharactersResponse,
     Error,
     CharactersResponse,
-    [ 'characters', string ]
+    [ 'characters', string, string, string ]
   >({
-    queryKey: [ 'characters', searchedName ],
+    queryKey: [ 'characters', searchedName, filteredSpecies, filteredStatus ],
     queryFn: ({ pageParam = 1 }) =>
-      fetchCharactersByPage({ pageParam, searchedName }),
+      fetchCharactersByPage({
+        pageParam,
+        searchedName,
+        filteredStatus,
+        filteredSpecies,
+      }),
     initialPageParam: 1,
     getNextPageParam: lastPage => {
-      if (!lastPage.info.next) return undefined;
+      if (!lastPage.info.next) {
+        return undefined;
+      }
       const url = new URL(lastPage.info.next);
       const nextPage = url.searchParams.get('page');
       return nextPage ? Number(nextPage) : undefined;
